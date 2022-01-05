7082d4b3ea015b7b0f094ebccbfaf1cf2b64d115
Iko Almas <90682984+IkoAlmasDevGame@users.noreply.github.com>
2022-01-05 14:04:34 +0700

    Delete IndexWebsite directory

--- IndexWebsite/connect/koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host = mysqli_connect("localhost", "root", "", "webindex");
-
-if($host){
-    echo "Database berhasil";
-}else{
-    echo "Database tidak berhasil";
-}
-?>
--- IndexWebsite/connect/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-// mengaktifkan session
-session_start();
- 
-// menghapus semua session
-session_destroy();
-
-header("location:../index.php");
-?>
